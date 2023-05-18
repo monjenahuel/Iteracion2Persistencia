bc2be9d942c1c4a5492ba64ebb79f141a21968d6
Nahuel Monje <102997844+monjenahuel@users.noreply.github.com>
2023-05-17 21:33:20 -0300

    Add files via upload

---
+++ models/carrera.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const carrera = sequelize.define('carrera', {
+    nombre: DataTypes.STRING
+  }, {});
+  
+
+  /*
+  //codigo de asociacion  (tiene muchos:)
+  carrera.associate = function(models) {
+  	carrera.hasMany(models.materia,  // Modelo al que pertenece
+    {
+      as: 'materia',                 // nombre de mi relacion
+      foreignKey: 'id_carrera'       // campo con el que voy a igualar 
+    })
+  };
+  ///////////////////////
+*/
+
+  return carrera;
+};
---
+++ models/materia.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const materia = sequelize.define('materia', {
+    nombre: DataTypes.STRING,
+    id_carrera: DataTypes.INTEGER
+  }, {});
+  materia.associate = function(models) {
+    
+
+  	//asociacion a carrera (pertenece a:)
+  	materia.belongsTo(models.carrera// modelo al que pertenece
+    ,{
+      as : 'Carrera-Relacionada',  // nombre de mi relacion
+      foreignKey: 'id_carrera'     // campo con el que voy a igualar
+    })
+  	/////////////////////
+
+
+
+  };
+  return materia;
+};
